9bf430a787cea4952b19115b397bb3d02771b2a5
Artyom <artyomsh01@yandex.ru>
2020-10-08 03:58:34 +0300

    new file

---
+++ SparseMatrix.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab3
+{
+    public class Matrix<T>
+    {
+        Dictionary<string, T> _matrix = new Dictionary<String,T>();
+        int maxX;
+        int maxY;
+    }
+
+
+
+}
